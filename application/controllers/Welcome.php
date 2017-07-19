ef7cb23d4142294cb0d11b6c19e0e531abc14d7d
Muhammad Zaky <muhammad.zaky.pnl@gmail.com>
2017-07-19 22:20:09 +0700

    add domain bakclink

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -33,7 +33,7 @@ class Welcome extends CI_Controller {
     }
 
     public function output($output = null) {
-        $this->load->view('example.php', (array) $output);
+        $this->load->view('show_data.php', (array) $output);
     }
 
     function unique_field_name($field_name) {
@@ -54,10 +54,10 @@ class Welcome extends CI_Controller {
 
     public function niche() {
         $c = new Grocery_CRUD();
-        
+
         $c->set_table('niche');
         $c->set_subject('Niche');
-        
+
         $this->output($c->render());
     }
 
@@ -92,4 +92,34 @@ class Welcome extends CI_Controller {
         $this->output($c->render());
     }
 
+    public function backlink() {
+        $c = new Grocery_CRUD();
+
+        $c->set_table('backlink');
+        $c->set_subject('Backlink');
+        
+        $c->change_field_type('type', 'enum', array('Comment', 'WEB 2.0', 'Link Profile', 'Redirect', 'Forum'));
+        $c->change_field_type('rating', 'enum', array('Very Good', 'Good', 'Not Bad'));
+        $c->change_field_type('email_verification', 'enum', array('yes', 'No'));
+        $c->unset_columns('email', 'username', 'password');
+        
+        
+        $this->output($c->render());
+    }
+    
+    public function no_link(){
+        return '#';
+    }
+
+        public function domain_backlink(){
+        $c = new Grocery_CRUD();
+        
+        $c->set_table('domain_backlink');
+        $c->set_relation('domain', 'domain', 'domain');
+        $c->set_relation('backlink', 'backlink', 'backlink');
+        $c->change_field_type('indexed', 'enum', array('yes', 'No'));
+//        $c->set_relation_n_n('backlink', 'domain_backlink', 'backlink', 'domain', 'backlink', 'backlink');
+        
+        $this->output($c->render());
+    }
 }
